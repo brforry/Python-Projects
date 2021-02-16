e1aa164c29671ae6f40c80438512a7ab071e4131
brforry <67486262+brforry@users.noreply.github.com>
2021-02-16 14:34:41 -0700

    Create file_data_manipulation.py

---
+++ file_data_manipulation.py
@@ -0,0 +1,6 @@
+import numpy as np, pandas as pd, math, scipy, scipy.constants as const
+from numpy import nan as NaN
+
+filename = 'engine_data'
+# Convert xlsx data file to csv file (you will need to verify the tab name)
+# data_xlsx = pd.read_excel('')
